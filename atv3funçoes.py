9d03615535c9b54a2efbf95df92ae54874d47922
jhonataconstantinobasilio <jhonataconstantinobasilio@gmail.com>
2025-08-06 11:45:25 -0300

    Add files via upload

---
+++ atv3funçoes.py
@@ -0,0 +1,8 @@
+numero = int(input("digite seu numero"))
+def eh_par(numero):
+    if numero %2==0:
+        print("é par")
+    else:
+        print("é impar")
+eh_par(numero)          
+    
---
+++ atv4funçoes.py
@@ -0,0 +1,8 @@
+
+n2=9
+n7=9
+
+def soma(n1,n2):
+    print(n1 + n2)
+
+soma(n2,n7)    
